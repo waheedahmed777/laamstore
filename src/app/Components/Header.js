e1996132fc9e15285e570bca5fdf7c56bd06a795
waheedahmed777 <waheedahmed6350@gmail.com>
2025-12-06 01:11:38 +0500

    update

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -1,46 +1,76 @@
 // app/components/Header.js
 'use client';
 import Image from 'next/image';
-import Link from 'next/link'; // ✔ Correct Import
+import Link from 'next/link';
 import React from 'react';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { CiBookmark } from 'react-icons/ci';
 import { useRouter } from 'next/navigation';
+import { FiUser } from 'react-icons/fi';
+import { HiBars3 } from 'react-icons/hi2';
+import { CiShoppingCart } from 'react-icons/ci';
+import { RxCross1 } from 'react-icons/rx';
+
 import './Header.css';
 
 export default function Header() {
   const router = useRouter();
+  const [open, setOpen] = React.useState(false); // 👈 Sidebar State
 
   return (
-    <div className='Header'>
-      <img src='/public/laam-logo.png' alt='' />
+    <>
+      <div className='Header'>
+        {/* Bottom Section */}
+        <div className='bottom'>
+          {/* Logo */}
+          <div className='logo'>
+            <img src='/svgexport-1 (1).png' alt='Logo' className='logo-img' />
+          </div>
 
-      <div className='menu'>
-        <ul>
-          <li>
-            {' '}
-            <Link href='/'>Home</Link>{' '}
-          </li>
-          <li>
-            {' '}
-            <Link href='/products'>Products</Link>
-          </li>
-          <li>
-            {' '}
-            <Link href='/about'>About</Link>{' '}
-          </li>
-          <li>
-            {' '}
-            <Link href='/contact'>Contact</Link>{' '}
-          </li>
-        </ul>
+          {/* Menu Icon */}
+          <div className='menu'>
+            <HiBars3 className='menu-icon' onClick={() => setOpen(true)} />
+          </div>
+        </div>
       </div>
 
-      <div className='icon'>
-        <FaShoppingCart size={30} color='black' />
-        <span>0</span>
+      {/* Right Sidebar / Popup */}
+      <div className={`sidebar ${open ? 'open' : ''}`}>
+        {/* Close Button */}
+        <button className='close-btn' onClick={() => setOpen(false)}>
+          <RxCross1 />
+        </button>
 
-        <FaUser onClick={() => router.push('/login')} size={30} color='black' />
+        {/* Top Icons in Sidebar */}
+        <div className='sidebar-top'>
+          <CiShoppingCart className='sidebar-icon' />
+          <FiUser className='sidebar-icon' />
+          <CiBookmark className='sidebar-icon' />
+        </div>
+
+        {/* Menu Links */}
+        <div className='sidebar-links'>
+          <Link className='link' href={'/'}>
+            {' '}
+            Home{' '}
+          </Link>
+          <Link className='link' href={'/products'}>
+            {' '}
+            Products{' '}
+          </Link>
+          <Link className='link' href={'/about'}>
+            {' '}
+            About{' '}
+          </Link>
+          <Link className='link' href={'/contact'}>
+            {' '}
+            Contact{' '}
+          </Link>
+          <Link className='link' href={'/blog'}>
+            {' '}
+            Blog{' '}
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -12,10 +12,23 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <head>
+        {/* Google Fonts */}
         <link
           href='https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;500;700&display=swap'
           rel='stylesheet'
         />
+
+        {/* Bootstrap CSS */}
+        <link
+          href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
+          rel='stylesheet'
+        />
+
+        {/* Bootstrap JS Bundle */}
+        <script
+          src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js'
+          defer
+        ></script>
       </head>
       <body>
         <Header />
